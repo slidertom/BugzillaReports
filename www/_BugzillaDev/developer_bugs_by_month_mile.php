c548f428f160992eaf5c4b01ad7357194e4f2f8b
Tomas <slidertom@gmail.com>
2020-01-02 13:19:24 +0200

    if there are no bugs -> do show month and year selection anyway.

--- www/_BugzillaDev/developer_bugs_by_month_mile.php
+++ www/_BugzillaDev/developer_bugs_by_month_mile.php
@@ -5,14 +5,15 @@ require_once 'developer_milestone_table.php';
 
 function developer_bugs_by_moth_by_mile($dbh, $users, $products, $developer_id, $year, $month)
 {
+	echo "<br>";
+	create_year_month_select_table($year, $month);
+	
 	$bugs = bugs_get_developer_month_bugs($dbh, $users, $products, $developer_id, $year, $month);
     if ( !$bugs ) {
 		echo "<h3>There are no bugs fixed.</h3>";
 		return;
 	}
 	
-	echo "<br>";
-	create_year_month_select_table($year, $month);
 	developer_milestone_bugs_to_table($bugs);
 }
 

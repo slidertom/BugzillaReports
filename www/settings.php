aeec623af64ef13f5b8f0bff3f09155e81fb659d
Tomas <slidertom@gmail.com>
2024-02-26 16:32:37 +0200

    clean-up

--- www/settings.php
+++ www/settings.php
@@ -1,8 +1,8 @@
 <?php
 /*
-	Copyright by Tomas Rapkauskas bugzilla_base/license.txt 
-	All rights reserved.
-	To use this component please contact slidertom@gmail.com to obtain a license.
+    Copyright by Tomas Rapkauskas bugzilla_base/license.txt 
+    All rights reserved.
+    To use this component please contact slidertom@gmail.com to obtain a license.
 */
 
 $g_logo_img = "";//"../res/logo.png";
@@ -10,24 +10,24 @@ $g_host     = php_uname("n");
 
 function get_logo_img_full_path()
 {
-	global $g_logo_img;
-	return $g_logo_img;
+    global $g_logo_img;
+    return $g_logo_img;
 }
 
 function get_server_host_name()
 {
-	global $g_host;
-	return $g_host;
+    global $g_host;
+    return $g_host;
 }
 
 function get_system_name()
 {
-	return "Bugzilla Reports";
+    return "Bugzilla Reports";
 }
 
 function get_version()
 {
-	return "3.0";
+    return "3.0";
 }
 
 ?>
